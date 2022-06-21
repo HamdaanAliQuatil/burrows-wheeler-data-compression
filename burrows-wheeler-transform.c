b43bc0b3a0fe3fbb0072d9ef3b9187163bd96857
Halyris <81964336+Halyris@users.noreply.github.com>
2022-06-21 17:20:55 +0530

    Removed size restriction by reallocating if needed

--- burrows-wheeler-transform.c
+++ burrows-wheeler-transform.c
@@ -1,63 +1,70 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "utility.h"
-  
-int sortRotation(const void* x, const void* y){
-    rotation* rx = (rotation*)x;
-    rotation* ry = (rotation*)y;
-    return strcmp(rx->suffix, ry->suffix);
+
+static int length = 0;
+
+int sortRotation(const void *x, const void *y) {
+    rotation *rx = (rotation *)x;
+    rotation *ry = (rotation *)y;
+    return memcmp(rx->suffix, ry->suffix, length);
 }
-  
-int* suffixArray(char* input_text, int length){
-    rotation suff[length];
-  
-    for (int i = 0; i < length; i++) {
+
+int *suffixArray(unsigned char *input_text, long length) {
+    rotation *suff = malloc(sizeof(rotation) * length);
+
+    for (long i = 0; i < length; i++) {
         suff[i].index = i;
         suff[i].suffix = (input_text + i);
     }
-  
 
     qsort(suff, length, sizeof(rotation), sortRotation);
 
-    int* suffix_array = (int*)malloc(length * sizeof(int));
+    int *suffix_array = malloc(length * sizeof(int));
 
-    for (int i = 0; i < length; i++)
+    for (long i = 0; i < length; i++)
         suffix_array[i] = suff[i].index;
-  
+
     return suffix_array;
 }
-  
-char* findLastChar(char* input_text, int* suffix_array, int n){
-    char* transformed_array= (char*)malloc(n * sizeof(char));
-    
-    int i;
+
+unsigned char *findLastChar(unsigned char *input_text, int *suffix_array, long n) {
+    unsigned char *transformed_array = malloc(n * sizeof(unsigned char));
+
+    long i;
     for (i = 0; i < n; i++) {
-        int j = suffix_array[i] - 1;
+        long j = suffix_array[i] - 1;
         if (j < 0)
             j = j + n;
-  
+
         transformed_array[i] = input_text[j];
     }
-  
+
     transformed_array[i] = '\0';
 
     return transformed_array;
 }
 
-int main(){
-    printf("Enter the string to perform Burrows Wheeler Transformation on: ");
+int main() {
+    long index = 0, input_size = 16;
+    unsigned char *input_text = malloc(sizeof(unsigned char) * input_size);
 
-    char input_text[100];
-    scanf("%s", input_text);
+    int ch;
+    while ((ch = getchar()) != EOF) {
+        input_text[index++] = ch;
+        if (index == input_size) {
+            input_size *= 2;
+            input_text = realloc(input_text, sizeof(unsigned char) * input_size);
+        }
+    }
 
-    int length = strlen(input_text);
-  
-    int* suffix_array= suffixArray(input_text, length);
+    length = index;
 
-    char* transformed_array= findLastChar(input_text, suffix_array, length);
-  
-    printf("Input text : %s\n", input_text);
-    printf("Burrows - Wheeler Transform : %s\n", transformed_array);
+    int *suffix_array = suffixArray(input_text, length);
+
+    unsigned char *transformed_array = findLastChar(input_text, suffix_array, length);
+
+    for (long i = 0; i < length; ++i) {
+        putchar(transformed_array[i]);
+    }
+
     return 0;
 }
